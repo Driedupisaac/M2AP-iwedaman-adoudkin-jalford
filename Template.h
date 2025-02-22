57cdc04464c32354412eeb6d3078aa4297f77b90
AlexDoudkin-SciAps <alexdoudkin@sciaps.com>
2025-02-21 19:21:52 -0500

    implemented my random function

--- Template.h
+++ Template.h
@@ -15,43 +15,60 @@ using namespace std;
 template <typename T>
 
 class Template {
-    vector <template> templates;
+    vector <T> templates;
 
 
- private:
-    std::vector<T> data;
+private:
 
-    //i got this method from geeks for geeks: the "fisher - yates" algorithm
-    void shuffle() {
-        //you dont need to shuffle a vector of size one.
-        if (data.size() <= 1) return;
+public:
 
-        //seeding the random generator
-        random_device rd;
-        mt19937 gen(rd());
+    vector<T> randomize(vector<T> originalVector) {
+        int originalVectorsize = originalVector.size();
+        stringstream ss;
+        const void* address;
+        vector<T> result(originalVector.size());
+        int memAddressSum;
+        //loops through the original vector getting memory addresses
+        while (originalVector.size() > 0) {
+            int i=0;
+            address = &originalVector[i];
+            ss << "0x" << std::hex << std::uppercase << std::setw(16) << std::setfill('0') << reinterpret_cast<uintptr_t>(address);
+            //loops through the memory address to get all digits and sums them together.
+            for (int j = 0; j < ss.str().size(); j++) {
+                if (isdigit(ss.str()[j])) {
+                    memAddressSum += int(ss.str()[j]);
+                }
+            }
+            //gets the curent time in milliseconds
+            auto now = std::chrono::system_clock::now();
+            auto duration = now.time_since_epoch();
+            auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count()%1000;
 
-        //for each item in the vector, then generate a random index, and swap (using std::swap)
-        //the indexes, and the data contained.
-        for (size_t i = data.size() - 1; i > 0; --i) {
-            uniform_int_distribution<size_t> dist(0, i);
-            size_t j = dist(gen);
-            swap(data[i], data[j]);
+            //adds sum of memory address integers to milisconds to get some more randomized number
+            int randomNumber = memAddressSum + milliseconds * 7;
+
+            //checks if the space in the vector is already taken
+            int testvar = result[randomNumber % originalVectorsize];
+            if (testvar == 0) {
+                result[randomNumber % originalVectorsize] = originalVector[i];
+            }else {
+                int k=0;
+                while (result[k] != 0) {
+                    auto startTime = chrono::system_clock::now();
+                    auto length = startTime.time_since_epoch();
+                    auto milliseconds = chrono::duration_cast<chrono::milliseconds>(length).count()%1000;
+                    k = milliseconds*7 % result.size();
+                }
+                result[k] = originalVector[i];
+            }
+            originalVector.erase(originalVector.begin());
+            i++;
         }
+
+        return result;
     }
 
 
-
-    //adoudkins stuff
-
-
-    void randomize() {
-      if (data.siza() <= 1) return;
-      auto now = std::chrono::system_clock::now();
-      auto duration = now.time_since_epoch();
-      auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-
-
-      }
 
 
 };
